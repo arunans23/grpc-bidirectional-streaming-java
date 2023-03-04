9a9a95d7419305e5e503725c7a9e49b1136c1266
Arunan Sugunakumar <arunans23@gmail.com>
2023-03-04 11:36:47 +0530

    Add implementation to send request from server

--- hogwarts-client/src/main/java/com/grpc/hogwarts/client/HogwartsClient.java
+++ hogwarts-client/src/main/java/com/grpc/hogwarts/client/HogwartsClient.java
@@ -1,18 +1,21 @@
 package com.grpc.hogwarts.client;
 
-import com.google.protobuf.Any;
-import com.google.protobuf.InvalidProtocolBufferException;
-import com.google.protobuf.StringValue;
-import com.grpc.hogwarts.service.Data;
-import io.grpc.*;
-import com.grpc.hogwarts.service.HogwartsServiceGrpc;
+import com.grpc.hogwarts.service.*;
+import io.grpc.Channel;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
-
 
 
 public class HogwartsClient {
 
     private HogwartsServiceGrpc.HogwartsServiceStub stub;
+    private StreamObserver<ClientData> requestObserver;
+    private String nodeId = getRandomNodeId();
+
+    private ElectronicList electronicList = populateElectronicList();
+    private VehicleList vehicleList = populateVehicleList();
 
     public HogwartsClient(Channel channel){
         stub = HogwartsServiceGrpc.newStub(channel);
@@ -22,47 +25,155 @@ public class HogwartsClient {
         HogwartsClient client = new HogwartsClient(channel);
 
         client.connect();
+        // how to wait client from terminating
+        Thread thread = new Thread(new HeartBeatService(client.requestObserver));
+        thread.run();
     }
 
 
     public void connect() {
-        StreamObserver<Data> responseObserver = new StreamObserver<Data>() {
+        //connect to server and send data based on the response received from the server
+        requestObserver = stub.connect(new StreamObserver<ServerData>() {
             @Override
-            public void onNext(Data data) {
-                Any any = data.getData();
-                if (any.is(StringValue.class)) {
-                    try {
-                        String receivedStr = any.unpack(StringValue.class).getValue();
-                        System.out.println("Client: "+receivedStr);
-                    } catch (InvalidProtocolBufferException ex) {
-                        throw new RuntimeException(ex);
-                    }
+            public void onNext(ServerData serverData) {
+                if (ServerData.ITEM.ELECTRONIC.equals(serverData.getItem())) {
+                    sendElectronicList();
+                } else if (ServerData.ITEM.VEHICLE.equals(serverData.getItem())) {
+                    sendVehicleList();
+                } else {
+                    System.out.println("Received from server: " + serverData);
                 }
             }
 
-
-            @Override
+                @Override
             public void onError(Throwable throwable) {
-                throwable.printStackTrace();
+                //on error
+                Status status = Status.fromThrowable(throwable);
+                System.out.println("Error: " + status);
             }
 
             @Override
             public void onCompleted() {
-                System.out.println("Client completed!");
+                //on completion
+                System.out.println("Server has completed sending us something");
             }
+        });
 
-        };
+        //sending Node Id
+        requestObserver.onNext(ClientData.newBuilder()
+                .setNode(Node.newBuilder()
+                        .setId(nodeId)
+                        .build())
+                .build());
+    }
 
-        StreamObserver<Data> requestObserver = stub.connect(responseObserver);
-        try {
+    private String getRandomNodeId() {
+        //get random number between 1 to 100
+        return String.valueOf((int) (Math.random() * 100));
+    }
 
-            Data data = Data.newBuilder().setData(Any.pack(StringValue.of("63vds73gs6"))).build() ;
-            requestObserver.onNext(data);
-            requestObserver.onCompleted();
-        }
-        catch (RuntimeException e) {
-            requestObserver.onError(e);
-            throw e;
+    private void sendElectronicList() {
+        requestObserver.onNext(ClientData.newBuilder()
+                .setElectronicList(electronicList)
+                .build());
+    }
+
+    private void sendVehicleList() {
+        requestObserver.onNext(ClientData.newBuilder()
+                .setVehicleList(vehicleList)
+                .build());
+    }
+
+    private ElectronicList populateElectronicList() {
+        Electronic electronic1 = Electronic.newBuilder()
+                .setName("Iphone 12")
+                .setType("PHONE")
+                .build();
+
+        Electronic electronic2 = Electronic.newBuilder()
+                .setName("Iphone 11")
+                .setType("PHONE")
+                .build();
+
+        Electronic electronic3 = Electronic.newBuilder()
+                .setName("Iphone 10")
+                .setType("PHONE")
+                .build();
+
+        Electronic electronic4 = Electronic.newBuilder()
+                .setName("Galaxy S10")
+                .setType("PHONE")
+                .build();
+
+        Electronic electronic5 = Electronic.newBuilder()
+                .setName("Galaxy S9")
+                .setType("PHONE")
+                .build();
+
+        ElectronicList electronicList = ElectronicList.newBuilder()
+                .addElectronics(electronic1)
+                .addElectronics(electronic2)
+                .addElectronics(electronic3)
+                .addElectronics(electronic4)
+                .addElectronics(electronic5)
+                .build();
+
+        return electronicList;
+    }
+
+    private VehicleList populateVehicleList() {
+        Vehicle vehicle1 = Vehicle.newBuilder()
+                .setName("Audi")
+                .setType("CAR")
+                .build();
+
+        Vehicle vehicle2 = Vehicle.newBuilder()
+                .setName("BMW")
+                .setType("CAR")
+                .build();
+
+        Vehicle vehicle3 = Vehicle.newBuilder()
+                .setName("Mercedes")
+                .setType("CAR")
+                .build();
+
+        Vehicle vehicle4 = Vehicle.newBuilder()
+                .setName("Honda")
+                .setType("CAR")
+                .build();
+
+        Vehicle vehicle5 = Vehicle.newBuilder()
+                .setName("Toyota")
+                .setType("CAR")
+                .build();
+
+        VehicleList vehicleList = VehicleList.newBuilder()
+                .addVehicles(vehicle1)
+                .addVehicles(vehicle2)
+                .addVehicles(vehicle3)
+                .addVehicles(vehicle4)
+                .addVehicles(vehicle5)
+                .build();
+
+        return vehicleList;
+    }
+}
+
+class HeartBeatService implements Runnable {
+    private StreamObserver<ClientData> requestObserver;
+    HeartBeatService(StreamObserver<ClientData> requestObserver) {
+        this.requestObserver = requestObserver;
+    }
+    public void run() {
+        while (true) {
+            try {
+                requestObserver.onNext(ClientData.newBuilder()
+                        .setHeartBeat(HeartBeat.newBuilder().build())
+                        .build());
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
--- hogwarts-service/src/main/java/com/grpc/hogwarts/service/HogwartsServer.java
+++ hogwarts-service/src/main/java/com/grpc/hogwarts/service/HogwartsServer.java
@@ -1,24 +1,105 @@
 
 package com.grpc.hogwarts.service;
 
+import com.google.protobuf.Empty;
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
+import io.grpc.Status;
+import io.grpc.stub.StreamObserver;
 
-import java.util.Scanner;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public class HogwartsServer {
+public class HogwartsServer  {
+    private static List<StreamObserver<ServerData>> observers = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
-        Server server = ServerBuilder.forPort(8080).addService(new HogwartsServiceImpl()).build();
+        // create a server and send data to client based on user input
+        Server server = ServerBuilder.forPort(8080)
+                .addService(new HogwartsServiceGrpc.HogwartsServiceImplBase() {
+                    @Override
+                    public StreamObserver<ClientData> connect(StreamObserver<ServerData> responseObserver) {
+                        observers.add(responseObserver);
+                        return new HogwartsServerObserver(responseObserver);
+                    }
+                })
+                .build();
         server.start();
-        System.out.println("Server started");
-        Scanner scanner = new Scanner(System.in);
-        TimeUnit.SECONDS.sleep(10);
-        HogwartsServiceImpl.broadcast("Test");
-        server.awaitTermination();
+        System.out.println("Server started on port: " + server.getPort());
 
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Received shutdown request");
+            try {
+                server.shutdown().awaitTermination(30, TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            System.out.println("Server has been shutdown");
+        }));
+
+
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter node ID");
+            int nodeId = scanner.nextInt();
+            if (observers.size() < nodeId) {
+                System.out.println("Node ID doesn't exist");
+                continue;
+            }
+            System.out.println("Enter 1 to send electronic list, 2 to send vehicle list, 3 to exit");
+            int choice = scanner.nextInt();
+            StreamObserver<ServerData> serverObserver = observers.get(nodeId - 1);
+            if (choice == 1) {
+                serverObserver.onNext(ServerData.newBuilder()
+                        .setItem(ServerData.ITEM.ELECTRONIC)
+                        .build());
+            } else if (choice == 2) {
+                serverObserver.onNext(ServerData.newBuilder()
+                        .setItem(ServerData.ITEM.VEHICLE)
+                        .build());
+            } else if (choice == 3) {
+                server.shutdown();
+                break;
+            }
+        }
+    }
+}
+
+class HogwartsServerObserver implements StreamObserver<ClientData> {
+    private StreamObserver<ServerData> serverObserver;
+    private String clientId;
+
+    public HogwartsServerObserver(StreamObserver<ServerData> serverObserver) {
+        this.serverObserver = serverObserver;
+    }
+    @Override
+    public void onNext(ClientData clientData) {
+        if (clientData.hasNode()) {
+            clientId = clientData.getNode().getId();
+            System.out.println("Received registration request from client : " + clientData.getNode().getId());
+        } else if (clientData.hasHeartBeat()) {
+            System.out.println("Received heartbeat from client : " + clientId);
+        } else if (clientData.hasVehicleList()) {
+            System.out.println("Received vehicle list from client : " + clientData.getVehicleList().toString());
+        } else if (clientData.hasElectronicList()) {
+            System.out.println("Received electronic list from client : " + clientData.getElectronicList().toString());
+        } else {
+            System.out.println("Received from client: " + clientData);
+        }
     }
 
+    @Override
+    public void onError(Throwable throwable) {
+        //on error
+        Status status = Status.fromThrowable(throwable);
+        System.out.println("Error: " + status);
+    }
+
+    @Override
+    public void onCompleted() {
+        //on completion
+        System.out.println("Client has completed sending us something");
+    }
 }
+
 
